03e83133cc137cf5b7602b56c9364c73c59def4d
Kevin Sander <kevintsander@gmail.com>
2023-06-26 00:24:37 -0400

    removed routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StockSearchComponent } from './stocks/stock-search/stock-search.component';
 
@@ -25,7 +24,6 @@ import { NgChartsModule } from 'ng2-charts';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
